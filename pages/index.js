b08458e08a7fe80f899b8e8c6e9658e29c17b4fc
ELMEHDI99 <mahdifaraa99@gmail.com>
2023-09-19 12:45:52 +0100

    task 006

--- pages/index.js
+++ pages/index.js
@@ -37,15 +37,14 @@ const Index = () => {
     <div className="container mx-auto px-4 py-8">
       <header className="text-center mb-8">
         <h1 className="text-3xl font-bold mb-2">
-        &nbsp &nbsp Free YouTube Thumbnail Downloader <br></br> 
+        &nbsp; &nbsp; Free YouTube Thumbnail Downloader <br></br> 
         </h1>
        <h2 className="text-3xl font-bold mb-2">
         Easily Save Thumbnails in PNG, JPG, 4K & 8K Formats</h2> 
         
         <p className="text-gray-600">
-        Youtube Thumbnail downloader is an easy tool <br></br>
-        which helps you to Download any Youtube thumbnail<br></br>
-         or to save your device in png, jpg, 4K & 8K format</p>
+        Youtube Thumbnail Downloader is an easy tool that helps you download any YouTube thumbnail for free! Instantly grab high-quality thumbnails in various formats. No login required. Explore and download with ease.
+        </p>
       </header>
       <div className="text-center">
         <input
