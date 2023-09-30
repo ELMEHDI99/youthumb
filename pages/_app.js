2230bd3f409dfc5ec57b402fa03dfc846a316a06
ELMEHDI99 <mahdifaraa99@gmail.com>
2023-09-30 18:39:41 +0100

    task 030

--- pages/_app.js
+++ pages/_app.js
@@ -4,20 +4,21 @@ import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Fragment>
-      <DefaultSeo
-        title="Youtube Thumbnail Downloader"
-        description="Download high-quality thumbnails from YouTube videos."
-        canonical="https://www.freeyoutubethumbnaildownloader.com/"
-        openGraph={{
-          url: "https://www.freeyoutubethumbnaildownloader.com/",
-          title: "Youtube Thumbnail Downloader",
-          description: "Download high-quality thumbnails from YouTube videos.",
-          site_name: "Youtube Thumbnail Downloader",
-        }}
-      />
-      <Component {...pageProps} />
-    </Fragment>
+<Fragment>
+  <DefaultSeo
+    title="Free YouTube Thumbnail Downloader | Youtube Thumbnail Grabber"
+    description="FreeYouTubeThumbnailDownloader.com is your free online tool for downloading YouTube video thumbnails in different sizes and qualities. Simply enter the video URL to save the thumbnail. Experience the best YouTube thumbnail download today!"
+    canonical="https://www.freeyoutubethumbnaildownloader.com/"
+    openGraph={{
+      url: "https://www.freeyoutubethumbnaildownloader.com/",
+      title: "Free YouTube Thumbnail Downloader | Youtube Thumbnail Grabber",
+      description: "FreeYouTubeThumbnailDownloader.com is your free online tool for downloading YouTube video thumbnails in different sizes and qualities. Simply enter the video URL to save the thumbnail. Experience the best YouTube thumbnail download today!",
+      site_name: "Free YouTube Thumbnail Downloader",
+    }}
+  />
+  <Component {...pageProps} />
+</Fragment>
+
   );
 }
 
